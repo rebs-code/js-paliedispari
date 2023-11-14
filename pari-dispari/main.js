e1b9cac06a6ce368468a1f86e9919a80765e92ed
rebs-code <jack.rebussi@gmail.com>
2023-11-14 15:49:08 +0100

    style: format js

--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 function randomNum() {
   const randomNumber = Math.floor(Math.random() * 5) + 1;
   console.log(`Il computer ha giocato ${randomNumber}`);
@@ -21,13 +20,17 @@ function result(sum) {
 const myNumber = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 const myChoice = prompt("Pari o dispari?");
 
-if (myNumber >= 1 && myNumber <= 5 && (myChoice === "pari" || myChoice === "dispari")) {
-    console.log(`Hai inserito il numero ${myNumber}`);
-    console.log(`Hai scelto ${myChoice}`);
-    const sum = myNumber + randomNum();
-    result(sum);
+if (
+  myNumber >= 1 &&
+  myNumber <= 5 &&
+  (myChoice === "pari" || myChoice === "dispari")
+) {
+  console.log(`Hai inserito il numero ${myNumber}`);
+  console.log(`Hai scelto ${myChoice}`);
+  const sum = myNumber + randomNum();
+  result(sum);
 } else {
-    console.log("Il numero inserito e/o la scelta non è valida. Inserisci un numero da 1 a 5 e scegli 'pari' o 'dispari'.");
+  console.log(
+    "Il numero inserito e/o la scelta non è valida. Inserisci un numero da 1 a 5 e scegli 'pari' o 'dispari'."
+  );
 }
-
-
